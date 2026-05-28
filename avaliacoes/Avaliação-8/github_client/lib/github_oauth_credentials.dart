6bb8a46165a6462b8dc572cb3dfec64950be4f29
Biiancabm <brumatibianca@gmail.com>
2026-05-28 17:28:41 -0300

    atividade 8

---
+++ avaliacoes/Avaliação-8/github_client/lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = 'Ov23libuBGImn1CN1Vsg';
+const githubClientSecret = '73f3b24c718c41a6b366ed73f15a6040616cac30';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
